ecb3cda3ac156c71654d5cf89948ed6c0bd13162
Khanh Tran <khanh.tranxuan@euroland.com>
2023-10-03 15:55:58 +0700

    Add sample project for use API demo

---
+++ Controllers/WebAPIs/SampleController.cs
@@ -0,0 +1,30 @@
+﻿using DotnetCoreVCB.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCoreVCB.Controllers.WebAPIs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<SimpleUser> users = new List<SimpleUser>();
+            users.Add(new SimpleUser()
+            {
+                UserName = "Khanh.tx@euroland.com",
+                Password = "password",
+                Roles = "Admin"
+            });
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult Post(SimpleUser user)
+        {
+            return Ok(user);
+        }
+    }
+}
